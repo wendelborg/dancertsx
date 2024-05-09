ddb12b62ee0066047d1ded9fde5dfb6cde90c27c
lobotommy <wendelborg@gmail.com>
2024-05-09 17:20:25 +0200

    SandDance is running OK with reverted react version

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as fluentui from '@fluentui/react';
+import * as vega from 'vega';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Explorer, use } from '@msrvida/sanddance-explorer';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//quickstart: use the included stylesheet
+import "@msrvida/sanddance-explorer/dist/css/sanddance-explorer.css"
+import "./index.css"
+
+fluentui.initializeIcons();
+
+use(fluentui, React, ReactDOM, vega as any);
+
+const data = [
+  { a: 1, b: "c1" },
+  { a: 1, b: "c2" },
+  { a: 2, b: "c3" },
+  { a: 3, b: "c4" }
+];
+
+const explorerProps = {
+  logoClickUrl: "https://microsoft.github.io/SandDance/",
+  mounted: (explorer: any) => {
+    explorer.load(data);
+  }
+};
+
+ReactDOM.render(React.createElement(Explorer, explorerProps), document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
